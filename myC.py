a70d3ad41eda0b5e267daeba2df4ec302459862c
seol <knmvh5@naver.com>
2023-03-20 13:37:52 +0900

    my Commit2 빼기 함수 추가

--- myC.py
+++ myC.py
@@ -1,8 +1,16 @@
 ##함수 선언부
 def add_func(n1, n2):
     return n1 + n2
+
+def add_func(n1, n2):
+    return n1 - n2
 ##전역 변수부
 num1, num2, result = 100, 200, 0
+
 ##메인 코드부
 result = add_func(num1, num2)
 print(num1, '+', num2, '=', result)
+
+result = sub_func(num1, num2)
+print(num1, '-', num2, '=', result)
+
