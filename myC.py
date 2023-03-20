9a77ef68150be9157d67740181ad5f3161b70f3e
seol <knmvh5@naver.com>
2023-03-20 13:41:36 +0900

    my Commint3 곱셈기 추가

--- myC.py
+++ myC.py
@@ -2,8 +2,11 @@
 def add_func(n1, n2):
     return n1 + n2
 
-def add_func(n1, n2):
+def sub_func(n1, n2):
     return n1 - n2
+
+def mux_func(n1, n2):
+    return n1 * n2
 ##전역 변수부
 num1, num2, result = 100, 200, 0
 
@@ -13,4 +16,9 @@ print(num1, '+', num2, '=', result)
 
 result = sub_func(num1, num2)
 print(num1, '-', num2, '=', result)
+
+result = mux_func(num1, num2)
+print(num1, '*', num2, '=', result)
+
+
 
