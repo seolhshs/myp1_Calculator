201b62cd9fde54dd01bee70a0cd70f799d08232d
seol <knmvh5@naver.com>
2023-03-20 13:43:27 +0900

    my Commit 4 나눗셈기 추가

--- myC.py
+++ myC.py
@@ -7,6 +7,9 @@ def sub_func(n1, n2):
 
 def mux_func(n1, n2):
     return n1 * n2
+
+def mu_func(n1, n2):
+    return n1 / n2
 ##전역 변수부
 num1, num2, result = 100, 200, 0
 
@@ -19,6 +22,9 @@ print(num1, '-', num2, '=', result)
 
 result = mux_func(num1, num2)
 print(num1, '*', num2, '=', result)
+
+result = mu_func(num1, num2)
+print(num1, '/', num2, '=', result)
 
 
 
